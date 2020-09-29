88b7d3d03addcb7ab5d5fde6a1d40598316a06e4
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-29 09:51:20 -0500

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+# This entrypoint file to be used in development. Start by reading README.md
+import time_series_visualizer
+from unittest import main
+
+# Test your function by calling it here
+time_series_visualizer.draw_line_plot()
+time_series_visualizer.draw_bar_plot()
+time_series_visualizer.draw_box_plot()
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
---
+++ time_series_visualizer.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+# Import data (Make sure to parse dates. Consider setting index column to 'date'.)
+df = None
+
+# Clean data
+df = None
+
+
+def draw_line_plot():
+    # Draw line plot
+
+
+
+
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('line_plot.png')
+    return fig
+
+def draw_bar_plot():
+    # Copy and modify data for monthly bar plot
+    df_bar = None
+
+    # Draw bar plot
+
+
+
+
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('bar_plot.png')
+    return fig
+
+def draw_box_plot():
+    # Prepare data for box plots (this part is done!)
+    df_box = df.copy()
+    df_box.reset_index(inplace=True)
+    df_box['year'] = [d.year for d in df_box.date]
+    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+
+    # Draw box plots (using Seaborn)
+
+
+
+
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('box_plot.png')
+    return fig
