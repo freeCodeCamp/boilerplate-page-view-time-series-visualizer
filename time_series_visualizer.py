029a9f2966a3c7b4f8f0c4cfe2d03b8f90505889
Hermann29 <118738312+Hermann29@users.noreply.github.com>
2024-01-18 09:46:19 +0100

    Update time_series_visualizer.py

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,10 +5,10 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'], index_col='date')
 
 # Clean data
-df = None
+df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
 
 
 def draw_line_plot():
@@ -24,7 +24,7 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df_cleaned.groupby([df_cleaned.index.year, df_cleaned.index.month]).mean().unstack()
 
     # Draw bar plot
 
